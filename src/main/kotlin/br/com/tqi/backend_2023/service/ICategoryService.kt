c720dc2253d20bdfee277db8a925522f069c0925
jribas-dev <3b3brazil@gmail.com>
2023-07-18 00:37:02 -0300

    category service

---
+++ src/main/kotlin/br/com/tqi/backend_2023/service/ICategoryService.kt
@@ -0,0 +1,10 @@
+package br.com.tqi.backend_2023.service
+
+import br.com.tqi.backend_2023.entity.Category
+
+interface ICategoryService {
+    fun save(category: Category): Category
+    fun findByCategoryId(categoryId: Int): Category
+    fun findAllByParent(parentId: Int?): Set<Category>
+    fun deleteByCategoryId(categoryId: Int): Map<String, String>
+}
---
+++ src/main/kotlin/br/com/tqi/backend_2023/service/impl/CategoryService.kt
@@ -0,0 +1,36 @@
+package br.com.tqi.backend_2023.service.impl
+
+import br.com.tqi.backend_2023.entity.Category
+import br.com.tqi.backend_2023.exception.BusinessException
+import br.com.tqi.backend_2023.repository.CategoryRepository
+import br.com.tqi.backend_2023.service.ICategoryService
+import org.springframework.stereotype.Service
+
+@Service
+class CategoryService(
+    private val categoryRepository: CategoryRepository
+): ICategoryService {
+
+    override fun save(category: Category): Category {
+        return this.categoryRepository.save(category)
+    }
+
+    override fun findByCategoryId(categoryId: Int): Category {
+        return this.categoryRepository.findById(categoryId).orElseThrow{
+            throw BusinessException("Category Id $categoryId not found")
+        }
+    }
+
+    override fun findAllByParent(parentId: Int?): Set<Category> {
+        return this.categoryRepository.findAllByParentId(parentId)
+    }
+
+    override fun deleteByCategoryId(categoryId: Int): Map<String, String> {
+        val category: Category = this.categoryRepository.findById(categoryId).orElseThrow {
+            throw BusinessException("Category Id $categoryId not exists")
+        }
+        this.categoryRepository.deleteById(categoryId)
+        return mutableMapOf("Category" to category.categoryName, "Message" to "Has been deleted")
+    }
+
+}
